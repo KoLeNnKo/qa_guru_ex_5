6a9b95b574be375c58b8f6b7fb871895d6cd6077
KoLeNnKo <152208162+KoLeNnKo@users.noreply.github.com>
2025-12-09 20:42:35 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,17 @@
 import pytest
 from selene import browser
-
+from selenium import webdriver
 
 @pytest.fixture(autouse=True)
 def settings_browser():
-    browser.config.driver_name = 'firefox'
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.timeout = 10
     browser.config.headless = False
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
 
     yield
 
